376b0b014b82eb1f442353d909fdc26b48a5e190
eugenioprincipi <eugenioprincipi@mbp-di-eugenio.lan>
2020-03-11 12:44:29 +0100

    Laboratorio 01, programma completato.

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -3,6 +3,8 @@ package it.polito.tdp.parole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -28,18 +30,76 @@ public class FXMLController {
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    private TextArea txtResultTempi;
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    	elenco.removeParola(this.txtResult.getSelectedText());
+    	
+    	this.txtResult.clear();
+    	LinkedList<String> elencoOrdinato = new LinkedList<String>();
+    	elencoOrdinato = (LinkedList<String>) elenco.getElenco();
+    	
+    	for(String s : elencoOrdinato)
+    		txtResult.appendText(s+"\n");
+    	
+    	this.txtResultTempi.appendText(Long.toString(System.nanoTime())+"\n");
+    	
+    	long tempi = System.nanoTime();
+    	txtResultTempi.appendText(String.valueOf(tempi+"\n"));
+    }
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	txtResult.clear();
+    	
+    	for(int i=0; i<txtParola.getText().length(); i++) {
+    		if(!Character.isAlphabetic(txtParola.getText().charAt(i))) {
+    			txtResult.appendText("Carattere non valido");
+    		}
+    	}
+    	
+    	String parolaDaInserire = txtParola.getText();
+    	
+    	//VERSIONE LINKEDLIST
+    	
+    	elenco.addParola(parolaDaInserire);  // posso farlo su un'unica riga btw
+    	
+    	LinkedList<String> elencoOrdinato = new LinkedList<String>();
+    	elencoOrdinato = (LinkedList<String>)elenco.getElenco();
+    	
+    	for(String s : elencoOrdinato)
+    		txtResult.appendText(s+"\n");
+    	
+    	//VERSIONE ARRAYLIST
+    	
+    	/*elenco.addParolaArray(parolaDaInserire);
+    	
+    	ArrayList<String> elencoArrayOrdinato = new ArrayList<String>();
+    	elencoArrayOrdinato = (ArrayList<String>) elenco.getElencoArray();
+    	
+    	for(String s : elencoArrayOrdinato)
+    		txtResult.appendText(s+"\n");*/
+    	
+    	long tempi = System.nanoTime();
+    	txtResultTempi.appendText(String.valueOf(tempi+"\n"));
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	txtResult.clear();
+    	elenco.reset();
+    	
+    	// Ha senso qui?
+    	/*long tempi = System.nanoTime();
+    	txtResultTempi.appendText(String.valueOf(tempi+"\n"));*/
+    	
+    	txtResultTempi.clear();
     }
 
     @FXML
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,51 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	
+	LinkedList<String> paroleDaOrdinare;
+	ArrayList<String> paroleArray;
 		
 	public Parole() {
-		//TODO
+		paroleDaOrdinare = new LinkedList<String>();
+		paroleArray = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		paroleDaOrdinare.add(p);
+	}
+	
+	public void addParolaArray(String p) {
+		paroleArray.add(p);
+	}
+	
+	public void removeParola(String p) {
+		paroleDaOrdinare.remove(p);
+	}
+	
+	public void removeParolaArray(String p) {
+		paroleArray.remove(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		LinkedList<String> ordinata = new LinkedList<String>(paroleDaOrdinare);
+		Collections.sort(ordinata);
+		return ordinata;
+	}
+	
+	public List<String> getElencoArray() {
+		ArrayList<String> ordinata = new ArrayList<String>(paroleArray);
+		Collections.sort(ordinata);
+		return ordinata;
 	}
 	
 	public void reset() {
-		// TODO
+		paroleDaOrdinare.clear();
+		paroleArray.clear();
 	}
 
 }
